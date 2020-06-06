e9a9b7dc9c884091f7a37cee78141b6a81d02335
phouchg <phouchg@protonmail.com>
2020-06-06 21:41:03 +0100

    fn. do.

--- fast.js
+++ fast.js
@@ -71,6 +71,63 @@ function analyzeIf(exp) {
   };
 }
 
+function analyzeFn(exp) {
+  const bodyLen = exp.length - 2; // rm fn tag and parms
+  let body = false;
+
+  if (bodyLen === 1) {
+    body = exp[2];
+  } else {
+    body = exp.slice(2);
+    body.unshift("do");
+  }
+
+  body = analyze(body);
+
+  const clo = {
+    parms: exp[1],
+    body: body,
+    parml: exp[1].indexOf(".") > -1 ? -1 : exp[1].length,
+  };
+
+  if (fnp(json)) {
+    clo.clo = true;
+  } else {
+    clo.mac = true;
+  }
+
+  return function (env) {
+    clo.env = env;
+    return clo;
+  };
+}
+
+function analyzeSeq(exps) {
+  function sequentially(proc1, proc2) {
+    return function (env) {
+      proc1(env);
+      return proc2(env);
+    };
+  }
+
+  const expsl = exps.length;
+
+  if (expsl === 0) {
+    throw new Error("empty seq");
+  }
+
+  const lastIndex = expsl - 1;
+  let proc = analyze(exps[0]);
+
+  for (let i = 1; i < expsl; i++) {
+    if (i === lastIndex) {
+      return proc;
+    }
+
+    proc = sequentially(proc, analyze(exps[i]));
+  }
+}
+
 function analyze(exp) {
   if (selfEvaluatingP(exp)) {
     return analyzeSelfEvaluating(exp);
@@ -92,6 +149,14 @@ function analyze(exp) {
 
   if (ifp(exp)) {
     return analyzeIf(exp);
+  }
+
+  if (fnp(exp) || macp(exp)) {
+    return analyzeFn(exp);
+  }
+
+  if (dop(exp)) {
+    return analyzeSeq(exp.slice(1));
   }
 }
 
